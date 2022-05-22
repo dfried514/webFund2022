8415e1261a590e138512333653633fead4947f15
dfried514 <dfried514@gmail.com>
2022-05-22 11:03:34 -0700

    add js script

---
+++ Friedman_David_Button_Clicker/script.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText = element.innerText === "Login" ? "Logout" : "Login";
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function liked() {
+    alert("Ninja was liked");
+}
